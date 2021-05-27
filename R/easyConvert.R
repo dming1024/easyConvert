436e52ecdc0a8db1107c8689010363d28202da82
fqiang1024 <39649878+fanqiang1024@users.noreply.github.com>
2021-05-27 09:51:30 +0800

    add ncbi parameter

--- R/easyConvert.R
+++ R/easyConvert.R
@@ -5,10 +5,10 @@
 #' @param queryList the list to be annotated
 #' @param queryType including `ENTREZID`,`SYMBOL`,`ENSEMBL`, correspond to the type of your `querList`
 #' @export
-easyConvert<-function(species=c("HUMAN","MUS"),queryList,queryType=c("ENTREZID","SYMBOL","ENSEMBL")){
+easyConvert<-function(species=c("HUMAN","MUS"),queryList,queryType=c("ENTREZID","SYMBOL","ENSEMBL","NCBI")){
   species <- match.arg(species)
   queryType <- match.arg(queryType)
- 
+
     if(species=="HUMAN"){
       results=eval(parse(text = sprintf("results=homodf[match(queryList,homodf$%s),]",queryType)))
       return(results)
@@ -17,5 +17,5 @@ easyConvert<-function(species=c("HUMAN","MUS"),queryList,queryType=c("ENTREZID",
       results=eval(parse(text = sprintf("results=musdf[match(queryList,musdf$%s),]",queryType)))
       return(results)
     }
-  
+
 }
